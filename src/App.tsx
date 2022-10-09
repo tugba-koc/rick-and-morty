4f43233d7a22e0caf1025a9ba6ec9bf1d131f256
tugba-uppercase <tugba@uppercasebrands.com>
2022-10-09 19:56:11 +0300

    create basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test';
+import { EpisodeProvider } from './context/EpisodeContext';
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Rich and Morty</h1>
+      <p>Select your favorites</p>
+      <EpisodeProvider>
+        <Test/>
+      </EpisodeProvider>
     </div>
   );
 }
